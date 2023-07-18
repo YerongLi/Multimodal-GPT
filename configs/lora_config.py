c66ccd436f6667e24cc6d37f1721e4ae2d979ef5
Yerong <yerong.li@outlook.com>
2023-07-18 02:53:21 -0500

    lora ranking set to 4

--- configs/lora_config.py
+++ configs/lora_config.py
@@ -1,8 +1,8 @@
 tuning_config = dict(
     lora=True,
     lora_target_modules=["q_proj", "k_proj", "v_proj", "o_proj", "to_q", "to_kv", "to_out", "ff.1", "ff.3"],
-    lora_r=16,
-    lora_alpha=16,
+    lora_r=4,
+    lora_alpha=4,
     lora_dropout=0.0,
     vis=True,
     unfrozen=[],
