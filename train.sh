2711dddcbec28e0e68bcf12eb3b5a120c1d816fa
Yerong Li <yerong.li@outlook.com>
2023-07-18 01:39:57 -0500

    Update train.sh

--- train.sh
+++ train.sh
@@ -1,7 +1,7 @@
 torchrun --nproc_per_node=8 mmgpt/train/instruction_finetune.py \
   --lm_path /scratch/yerong/.cache/pyllama/hf/7B \
   --tokenizer_path /scratch/yerong/.cache/pyllama/hf/7B \
-  --pretrained_path checkpoints/OpenFlamingo-9B/checkpoint.pt \
+  --pretrained_path openflamingo/OpenFlamingo-9B \
   --run_name train-my-gpt4 \
   --learning_rate 1e-5 \
   --lr_scheduler cosine \
