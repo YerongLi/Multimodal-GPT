f50af8dcc795760bf9da2670de76a825621b4c82
Yerong <yerong.li@outlook.com>
2023-07-18 02:41:26 -0500

    Upload train

--- configs/dataset_config.py
+++ configs/dataset_config.py
@@ -1,12 +1,12 @@
 visual_datasets = [
     dict(
-        type="llava_dial",
+        type="llava",
         vis_root="data/coco/train2017",
         ann_paths=[
-            "data/llava/conversation_58k.json",
+            "data/llava/detail_23k.json",
+            "data/llava/complex_reasoning_77k.json",
         ],
     ),
-    ]
 
 language_datasets = [
     dict(
