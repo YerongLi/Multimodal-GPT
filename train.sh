6fe6b7d42d6ee465c7d46c026a618f7b43e01022
Yerong Li <yerong.li@outlook.com>
2023-07-18 01:46:02 -0500

    Update train.sh

--- train.sh
+++ train.sh
@@ -5,6 +5,6 @@ torchrun --nproc_per_node=8 mmgpt/train/instruction_finetune.py \
   --run_name train-my-gpt4 \
   --learning_rate 1e-5 \
   --lr_scheduler cosine \
-  --batch_size 1 \ 
+  --batch_size 1\ 
   --tuning_config configs/lora_config.py \
   --dataset_config configs/dataset_config.py
